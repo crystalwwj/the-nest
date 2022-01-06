9faf576cf3bcb5b879428781219a10b7760ea434
crystalwwj <b04901011@ntu.edu.tw>
2022-01-06 22:37:05 +0800

    create project

---
+++ blog/2022-01-06-Nest-opening.md
@@ -0,0 +1,14 @@
+---
+slug: grand-opening
+title: Grand Opening!
+authors: [crystal, steven]
+tags: [intro, rambles]
+---
+
+Hi there! This is the very first blog post of The Nest.
+
+I've been thinking about setting up a personal website so that I can access my notes from anywhere. I love my note taking apps but sometimes it's annoying that I only have my valuable stuff on my working computer. Besides, my partner and I wanted to start keeping docs on somewhere other than Google Drive, both as motivation to learn more stuff and keep record of our progress.
+
+Anyway, this website is probably going to contain random posts and notes for all kinds of stuff. I'll mainly work on cybersecurity and things I recently encountered on work, while Steven might be documenting some of his own studies. We hope to also continue our language learning endeavors. And probably some travel blogs or rambles.
+
+That's it for now!
---
+++ docusaurus.config.js
@@ -0,0 +1,101 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'The Nest',
+  tagline: 'Scrambled rambles',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'The Nest', // Usually your GitHub org/user name.
+  projectName: 'The Nest', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/crystalwwj/the-nest/edit/main',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/crystalwwj/the-nest/edit/main',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'The Nest',
+        logo: {
+          alt: 'The Nest',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Notes',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/crystalwwj/the-nest/edit/mainfacebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/cymetrics',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} The Nest, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
